be352545ad3cad0638fdbd666d18bb6c99c30789
brianl02 <63929731+brianl02@users.noreply.github.com>
2024-10-28 17:51:27 -0700

    additional comments

--- Obscura/scripts/camera_controllers/auto_scroll.gd
+++ Obscura/scripts/camera_controllers/auto_scroll.gd
@@ -19,28 +19,33 @@ func _process(delta: float) -> void:
 	if draw_camera_logic:
 		draw_logic()
 	
-	var tpos = target.global_position
-	var cpos = global_position
-	
 	var travel_distance = delta * autoscroll_speed
 	global_position.x += travel_distance.x
 	global_position.y += travel_distance.y
 	global_position.z += travel_distance.z
 	
-	#var diff_between_left_edges = (tpos.x - target.WIDTH / 2.0) - (cpos.x - box_width / 2.0)
-	#if diff_between_left_edges < 0:
+	var tpos = target.global_position
+	var cpos = global_position
+	
+	#left
+	var diff_between_left_edges = (tpos.x - target.WIDTH / 2.0) - (cpos.x - top_left.x)
+	if diff_between_left_edges < 0:
+		#move player as left edge pushes them
 		#global_position.x += diff_between_left_edges
-	##right
-	#var diff_between_right_edges = (tpos.x + target.WIDTH / 2.0) - (cpos.x + box_width / 2.0)
-	#if diff_between_right_edges > 0:
+	#right
+	var diff_between_right_edges = (tpos.x + target.WIDTH / 2.0) - (cpos.x + bottom_right.x)
+	if diff_between_right_edges > 0:
+		#negate players rightward movement so they do not leave the camera box
 		#global_position.x += diff_between_right_edges
-	##top
-	#var diff_between_top_edges = (tpos.z - target.HEIGHT / 2.0) - (cpos.z - box_height / 2.0)
-	#if diff_between_top_edges < 0:
+	#top
+	var diff_between_top_edges = (tpos.z - target.HEIGHT / 2.0) - (cpos.z - top_left.y)
+	if diff_between_top_edges < 0:
+		#negate players upward movement
 		#global_position.z += diff_between_top_edges
-	##bottom
-	#var diff_between_bottom_edges = (tpos.z + target.HEIGHT / 2.0) - (cpos.z + box_height / 2.0)
-	#if diff_between_bottom_edges > 0:
+	#bottom
+	var diff_between_bottom_edges = (tpos.z + target.HEIGHT / 2.0) - (cpos.z + top_left.y)
+	if diff_between_bottom_edges > 0:
+		#negate players downward movement
 		#global_position.z += diff_between_bottom_edges
 	
 	
